17df0deaea03af5a89f5bd240f5548ae8df20950
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-13 13:50:23 +0800

    added __unicode__() to establish good representation of objects

--- theses_sys/models.py
+++ theses_sys/models.py
@@ -4,29 +4,41 @@ class Researcher(models.Model):
 	first_name = models.CharField(max_length=255)
 	middle_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.last_name + ', ' + self.first_name + ' ' + self.middle_name[0] + '.'
 
 class Department(models.Model):
-	acronym = models.CharField(max_length=10)
-	description = models.CharField(max_length=255)
+	name = models.CharField(max_length=255)
 	email = models.EmailField()
 	contact_number = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.name
 
 class FacultyReg(models.Model):
 	username = models.CharField(max_length=255)
 	password = models.CharField(max_length=255)
 	add_date = models.DateTimeField(auto_now_add=True)
+	def __unicode__(self):
+		return self.name
 
 class FacultyProfile(models.Model):
 	id = models.ForeignKey(FacultyReg, primary_key=True, on_delete=models.CASCADE)
 	first_name = models.CharField(max_length=255)
 	middle_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
+	department = models.ForeignKey(Department)
+	def __unicode__(self):
+		return self.last_name + ', ' + self.first_name + ' ' + self.middle_name[0] + '.'
 
 class Tag(models.Model):
 	name = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.name
 
 class Category(models.Model):
 	name = models.CharField(max_length=255)
+	def __unicode__(self):
+		return self.name
 
 class Thesis(models.Model):
 	title = models.CharField(max_length=255)
@@ -38,6 +50,8 @@ class Thesis(models.Model):
 	add_date = models.DateTimeField(auto_now_add=True)
 	pub_date = models.DateTimeField()
 	acc_date = models.DateTimeField()
+	def __unicode__(self):
+		return self.title
 
 class Tags_Added(models.Model):
 	tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
