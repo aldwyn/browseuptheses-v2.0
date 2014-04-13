db0f6c2f9311db505e93d8331ea48c977d36dee8
AldwynForTheWin <elephunkies101@gmail.com>
2014-04-13 17:09:17 +0800

    added template for adding theses

--- browseuptheses/urls.py
+++ browseuptheses/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'browseuptheses.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^theses_sys/', include('theses_sys.urls', namespace='theses_sys')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- theses_sys/views.py
+++ theses_sys/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from theses_sys.models import Thesis, FacultyProfile, Researcher, Department, Tag, Category
 
-# Create your views here.
+def index(request):
+	theses = Thesis.objects.all().order_by('title')
+	return render(request, 'theses_sys/index.html', {'theses': theses})
+
+def add_thesis(request):
+	return render(request, 'theses_sys/add_thesis.html')
